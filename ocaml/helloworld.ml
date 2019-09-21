74b401d0516df6df9dbe3976f5a766d2b1eb54a1
yoowonsuk <sam2yuk@naver.com>
2019-09-21 13:06:45 +0900

    ocaml and postgreslq first update

---
+++ ocaml/helloworld.ml
@@ -0,0 +1,24 @@
+let hello = "hello";;
+let world = "world";;
+let helloworld = hello ^ " " ^ world;;
+let _ = print_endline helloworld;;
+
+1+2*3;;
+
+7/3;;
+7.0/.float_of_int 3;;
+not true;; (* works *)
+(* neg true;; wrong syntax *)
+
+
+3 = 3;;
+4 <> 4;;
+
+3 + int_of_float 2.0;; (* cannot express different types of value *)
+1.2 +. 3.5;;
+
+'c';;
+"cose212";;
+();;
+
+(* neg (-1);; neg는 함수 음수전달에는 괄호*)
